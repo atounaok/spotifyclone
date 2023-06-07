1ecbae8748356b658b8fd6dcf82075bdbca1dc9e
atounaok <atounaok@gmail.com>
2023-06-07 12:05:49 -0400

    :hammer: Sidebar & layout implementation

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+import { Figtree } from 'next/font/google'
+import Sidebar from '@/components/Sidebar'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Spotify',
+  description: 'Listen to music with spotify',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Sidebar>
+          {children}
+        </Sidebar>
+      </body>
+    </html>
+  )
+}
