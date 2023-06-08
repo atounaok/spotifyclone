07135679bb13926c82b661759d62bebeff394450
atounaok <atounaok@gmail.com>
2023-06-08 00:22:37 -0400

    :hammer: Input component

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 
 import Modal from "@/components/Modal";
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 
 const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -19,6 +20,7 @@ const ModalProvider = () => {
   return (
     <>
         <AuthModal />
+        <UploadModal/>
     </>
   )
 }
