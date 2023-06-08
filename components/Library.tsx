1bb0eba7f507f8eea23d65e54e7cb7170da5bc69
atounaok <atounaok@gmail.com>
2023-06-08 11:37:28 -0400

    :hammer: Show songs

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,8 +3,14 @@ import { AiOutlinePlus } from 'react-icons/ai'
 import useAuthModal from '@/hooks/useAuthModal'
 import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUploadModal.tsx';
+import { Song } from '@/types';
+import MediaItem from './MediaItem';
 
-const Library = () => {
+interface LibraryProps{
+    songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
@@ -31,7 +37,11 @@ const Library = () => {
                 size={20} className='text-neutral-400 cursor-pointer hover:text-white transition'/>
         </div>
         <div className='flex flex-col gap-y-2 mt-4 px-3'>
-
+            {songs.map((item) => (
+                <MediaItem onClick={() => {}}
+                    key={item.id}
+                    data={item}/>
+            ))}
         </div>
     </div>
   )
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "nuldtogadtuilcrtijfk.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
