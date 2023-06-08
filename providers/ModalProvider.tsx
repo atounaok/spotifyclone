927f1ec3053bce278de73e153fe9dbb073dfd9c4
atounaok <atounaok@gmail.com>
2023-06-07 23:18:04 -0400

    :hammer: ModalProvider preparation

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+import Modal from "@/components/Modal";
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if(!isMounted){
+        return null;
+    }
+
+  return (
+    <>
+        <Modal 
+            title="Test Modal"
+            description="Test description"
+            isOpen
+            onChange={() => {}}>
+                Test Children
+            </Modal>
+    </>
+  )
+}
+
+export default ModalProvider
