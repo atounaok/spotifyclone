bdcdf14206137eaa8bd6402917517440287e0a0d
atounaok <atounaok@gmail.com>
2023-06-07 22:55:32 -0400

    :hammer: UserProvider implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import './globals.css'
 import { Figtree } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Spotify',
+  title: 'Spotify clone',
   description: 'Listen to music with spotify',
 }
 
@@ -17,9 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   )
--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,22 +1,27 @@
 'use client';
 
-import { Database } from "@/types_db";
-import { createClientComponentClient, SupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
+
+import { Database } from "@/types_db";
 
 interface SupabaseProviderProps {
     children: React.ReactNode
 };
 
-const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
-    const [supabaseClient] = useState(() => createClientComponentClient<Database>());
-
-    return(
-        <SessionContextProvider supabaseClient={supabaseClient}>
-            {children}
-        </SessionContextProvider>
-    )
-}
-
-export default SupabaseProvider;
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
+    children
+  }) => {
+      const [supabaseClient] = useState(() =>
+      createClientComponentClient<Database>()
+    );
+  
+    return ( 
+      <SessionContextProvider supabaseClient={supabaseClient}>
+        {children}
+      </SessionContextProvider>
+    );
+  }
+   
+  export default SupabaseProvider;
